e0e55491faedbccb480d8b2230c9b5b25464a6f5
Nicolas Olenev <cardinal2113@gmail.com>
2021-10-30 14:13:51 +0300

    Исправлены орфографические ошибки

--- marathon/calc/calc.js
+++ marathon/calc/calc.js
@@ -4,39 +4,39 @@
 // Ваш калькулятор должен возвращать сообщение“ Error” в том случае если вы не указали все 3 параметра или если вычисляемые значения оказались не числами.
 // В случае с неверным“ идентификатором нужного действия” возвращайте“ unknown operation”
 
-
-
 // Решение
 
 // createCalc(); // Запуск скрипта (калькулятора)
 
-
-
 // Функция для вызова калькулятора, ввода данных, вывода ответа
 function createCalc() {
   while (true) {
-
     let a;
     let b;
     let operation;
 
     while (true) {
-      operation = prompt('Введите операцию из списка:\nsum(Сложение)\ndif(Вычетание)\nmulti(Умножение)\ndiv(Деление)\nrem(Взятие остатка от деления)\nexp(Возведение в степень)');
+      operation = prompt(
+        "Введите операцию из списка:\nsum(Сложение)\ndif(Вычитание)\nmulti(Умножение)\ndiv(Деление)\nrem(Взятие остатка от деления)\nexp(Возведение в степень)"
+      );
       if (operation === null) return; // Выбрать отмену = закрыть калькулятор
-      if (!checkOperation(operation)) { // Проверка на существование операции
-        alert('unknown operation');
+      if (!checkOperation(operation)) {
+        // Проверка на существование операции
+        alert("unknown operation");
         continue;
       }
-      a = prompt('Введите первое число:')
+      a = prompt("Введите первое число:");
       if (a === null) return; // Выбрать отмену = закрыть калькулятор
-      if (!checkIsRightNumber(a)) { // Проверка на правильность ввода числа
-        alert('Error');
+      if (!checkIsRightNumber(a)) {
+        // Проверка на правильность ввода числа
+        alert("Error");
         continue;
       }
-      b = prompt('Введите второе число:')
+      b = prompt("Введите второе число:");
       if (b === null) return;
-      if (!checkIsRightNumber(b)) { // Проверка на правильность ввода числа
-        alert('Error');
+      if (!checkIsRightNumber(b)) {
+        // Проверка на правильность ввода числа
+        alert("Error");
         continue;
       }
       break;
@@ -44,10 +44,7 @@ function createCalc() {
 
     alert(calc(operation, a, b)); // Вывод ответа
   }
-
 }
-
-
 
 //  Калькулятор
 function calc(operation, a, b) {
@@ -55,66 +52,79 @@ function calc(operation, a, b) {
   a = +a; // Явное преобразование типа в Number
   b = +b;
   switch (operation) {
-    case 'sum':
-    case 'сложение':
+    case "sum":
+    case "сложение":
       return getSum(a, b);
-    case 'dif':
-    case 'вычетание':
+    case "dif":
+    case "вычитание":
       return getDif(a, b);
-    case 'multi':
-    case 'умножение':
+    case "multi":
+    case "умножение":
       return getMulti(a, b);
-    case 'div':
-    case 'деление':
+    case "div":
+    case "деление":
       return getDiv(a, b);
-    case 'rem':
-    case 'взятие остатка от деления':
+    case "rem":
+    case "взятие остатка от деления":
       return getRem(a, b);
-    case 'exp':
-    case 'возведение в степень':
+    case "exp":
+    case "возведение в степень":
       return getExp(a, b);
   }
 }
 
-
-
 // Проверки на правильность вводимых данных
 function checkOperation(operation) {
-  return ['sum', 'dif', 'multi', 'div', 'rem', 'exp', 'сложение', 'вычетание', 'умножение', 'деление', 'взятие остатка от деления', 'возведение в степень'].includes(operation.trim().toLowerCase())
+  return [
+    "sum",
+    "dif",
+    "multi",
+    "div",
+    "rem",
+    "exp",
+    "сложение",
+    "вычитание",
+    "умножение",
+    "деление",
+    "взятие остатка от деления",
+    "возведение в степень"
+  ].includes(operation.trim().toLowerCase());
 }
 
 function checkIsRightNumber(a) {
-  return a !== '' && !isNaN(a);
+  return a !== "" && !isNaN(a);
 }
-
-
 
 //  Операции
-function getSum(a, b) { //   Сложение +
-  return a + b
+function getSum(a, b) {
+  //   Сложение +
+  return a + b;
 }
 
-function getDif(a, b) { //   Вычитание -
-  return a - b
+function getDif(a, b) {
+  //   Вычитание -
+  return a - b;
 }
 
-function getMulti(a, b) { //   Умножение *
-  return a * b
+function getMulti(a, b) {
+  //   Умножение *
+  return a * b;
 }
 
-function getDiv(a, b) { ///   Деление /
-  return a / b
+function getDiv(a, b) {
+  ///   Деление /
+  return a / b;
 }
 
-function getRem(a, b) { ///   Взятие остатка от деления %
-  return a % b
+function getRem(a, b) {
+  ///   Взятие остатка от деления %
+  return a % b;
 }
 
-function getExp(a, b) { ///   Возведение в степень **
-  return a ** b
+function getExp(a, b) {
+  ///   Возведение в степень **
+  return a ** b;
 }
-
-
 
 //  Тест вызовов калькулятора
 // console.log(`Сложение 40 и 2: ${calc('sum', 40, 2)}`)
