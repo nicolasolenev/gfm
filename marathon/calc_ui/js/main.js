9a6c2a16c7294508eb0da6e51d86841ce51ec44a
Nicolas Olenev <cardinal2113@gmail.com>
2021-11-27 15:35:05 +0300

    review v1.6

--- marathon/calc_ui/js/main.js
+++ marathon/calc_ui/js/main.js
@@ -1,149 +1,117 @@
-function calc(operation, a, b) {
-  if (operation === undefined || a === undefined || b === undefined)
-    return "Error";
+document.addEventListener("DOMContentLoaded", function () {
 
-  const operations = {
-    '+': () => +a + +b,
-    '–': () => a - b,
-    '×': () => a * b,
-    '÷': () => a / b
-  };
+  function calc(operation, a, b) {
+    if (operation === undefined || a === undefined || b === undefined)
+      return "Error";
 
-  if (!(operation in operations)) return "unknown operation";
-
-  const result = operations[operation]();
-
-  return isFinite(result) ? result : "Error";
-}
-
-
-const buttons = document.body.querySelectorAll('.button_num');
-const operations = document.body.querySelectorAll('.button_operation');
-const result = document.body.querySelector('.button_result');
-const deleteC = document.body.querySelector('.button_delete');
-const deleteBackspace = document.body.querySelector('.button_backspace');
-let output = document.body.querySelector('.calculator__output');
-let operation;
-let a = null;
-let b = null;
-let res = null;
-let checkresult = false;
-
-
-
-for (let button of buttons) {
-  button.onclick = function () {
-    if (res) {
-      res = null;
-      a = null;
-      output.innerHTML = null;
+    const operations = {
+      '+': () => +a + +b,
+      '–': () => a - b,
+      '×': () => a * b,
+      '÷': () => a / b
     }
-    if (!a) {
-      console.log('a: ' + a + ' b: ' + b + ' oper: ' + operation + ' check: ' + checkresult + ' res: ' + res);
-      output.innerHTML === '0' ? output.innerHTML = button.innerHTML : output.innerHTML += button.innerHTML;
-    }
-    if (a) {
-      if (!b) {
+
+    if (!(operation in operations)) return "unknown operation";
+    const result = operations[operation]();
+    return isFinite(result) ? result : "Error";
+  }
+
+
+  const buttons = document.body.querySelectorAll('.button_num');
+  const operations = document.body.querySelectorAll('.button_operation');
+  const equalSign = document.body.querySelector('.button_result');
+  const deleteSign = document.body.querySelector('.button_delete');
+  const backspace = document.body.querySelector('.button_backspace');
+  const output = document.body.querySelector('.calculator__output');
+  let operation = null;
+  let a = null;
+  let b = null;
+  let result = null;
+
+
+  for (let button of buttons) {
+    button.onclick = function () {
+      if (result !== null) {
+        result = null;
+        a = null;
+        b = null;
+        output.innerHTML = null;
+      }
+      if (a === null) {
+        output.innerHTML === '0' ? output.innerHTML = button.innerHTML : output.innerHTML += button.innerHTML;
+      } else if (b === null) {
         b = output.innerHTML = button.innerHTML;
-        console.log('a: ' + a + ' b: ' + b + ' oper: ' + operation + ' check: ' + checkresult + ' res: ' + res);
       } else {
-        b = output.innerHTML += button.innerHTML;
-        console.log('a: ' + a + ' b: ' + b + ' oper: ' + operation + ' check: ' + checkresult + ' res: ' + res);
+        output.innerHTML === '0' ? output.innerHTML = button.innerHTML : output.innerHTML += button.innerHTML;
+        b = output.innerHTML;
+      }
+      if (output.innerHTML.length >= 5) {
+        output.innerHTML = output.innerHTML.slice(0, 5);
       }
     }
-    if (output.innerHTML.length >= 5) {
-      output.innerHTML = output.innerHTML.slice(0, 5);
-    }
-    // if (output.innerHTML.length > 6) {
-    //   output.style.fontSize = '60px';
-    //   if (output.innerHTML.length > 9) {
-    //     output.innerHTML = output.innerHTML.slice(0, 9);
-    //   }
-    // } else output.style.fontSize = '96px';
   }
-}
 
 
-for (let oper of operations) {
-  oper.onclick = function () {
-    res = null;
-    if (checkresult) b = null;
-    checkresult = false;
-    if (a && b && operation) {
-      console.log('bem');
-      output.innerHTML = calc(operation, a, b);
-      a = output.innerHTML;
+  for (let oper of operations) {
+    oper.onclick = function () {
+      if (result !== null) {
+        b = null;
+        result = null;
+      } else if (a !== null && b !== null && operation !== null) {
+        output.innerHTML = calc(operation, a, b);
+        a = output.innerHTML;
+        b = null;
+      }
+
+      operation = oper.innerHTML;
+
+      if (a === null) {
+        a = output.innerHTML;
+      }
+    }
+  }
+
+
+  equalSign.onclick = function () {
+    if (a !== null && b !== null && operation !== null) {
+      result = output.innerHTML = calc(operation, a, b);
+      if (!Number.isInteger(result) && typeof result !== 'string') {
+        output.innerHTML = result.toFixed(2);
+      }
+
+      a = result;
+
+      if (output.innerHTML.length > 5) {
+        output.innerHTML = (+output.innerHTML).toExponential(1);
+      }
+    }
+  }
+
+
+  deleteSign.onclick = function () {
+    output.innerHTML = 0;
+    a = null;
+    b = null;
+    operation = null;
+    result = null;
+  }
+
+
+  backspace.onclick = function () {
+    if (output.innerHTML.length === 1) {
+      output.innerHTML = 0;
+    } else output.innerHTML = output.innerHTML.slice(0, output.innerHTML.length - 1);
+
+    if (a !== null) {
+      b = output.innerHTML;
+    }
+
+    if (result !== null) {
+      result = null;
+      a = null;
       b = null;
       operation = null;
     }
-
-    operation = oper.innerHTML;
-    if (!a) {
-      a = output.innerHTML;
-    }
-    console.log('a: ' + a + ' b: ' + b + ' oper: ' + operation + ' check: ' + checkresult + ' res: ' + res);
-    // output.innerHTML = null;
-    if (a && b && checkresult) {
-      output.innerHTML = calc(operation, a, b);
-      a = output.innerHTML;
-      console.log('a: ' + a + ' b: ' + b + ' oper: ' + operation + ' check: ' + checkresult + ' res: ' + res);
-      b = null;
-    }
-  }
-}
-
-
-
-result.onclick = function () {
-  if (a && b && operation) {
-    res = output.innerHTML = calc(operation, a, b);
-    if (!Number.isInteger(res)) {
-      output.innerHTML = res.toFixed(2);
-    }
-    a = res;
-    checkresult = true;
-    console.log('a: ' + a + ' b: ' + b + ' oper: ' + operation + ' check: ' + checkresult + ' res: ' + res);
-    if (output.innerHTML.length > 5) {
-      output.innerHTML = (+output.innerHTML).toExponential(1);
-    }
-  }
-  // if (output.innerHTML.length > 6) {
-  //   output.style.fontSize = '60px';
-  //   if (output.innerHTML.length > 9) {
-  //     output.innerHTML = output.innerHTML.slice(0, 9);
-  //   }
-  // } else output.style.fontSize = '96px';
-}
-
-
-
-deleteC.onclick = function () {
-  output.innerHTML = 0;
-  a = null;
-  b = null;
-  res = null;
-  operation = null;
-  console.log('a: ' + a + ' b: ' + b + ' oper: ' + operation + ' check: ' + checkresult + ' res: ' + res);
-}
-
-
-
-deleteBackspace.onclick = function () {
-  if (output.innerHTML.length === 1) {
-    output.innerHTML = 0;
-  } else {
-    output.innerHTML = output.innerHTML.slice(0, output.innerHTML.length - 1);
   }
 
-  if (res) {
-    output.innerHTML = 0;
-    res = null;
-    a = null;
-    b = null;
-  }
-
-  if (a) {
-    b = output.innerHTML;
-  }
-  console.log('a: ' + a + ' b: ' + b + ' oper: ' + operation + ' check: ' + checkresult + ' res: ' + res);
-}
+});
