d068ef35ba28e2cc07104af8b319b65a0b8cf3eb
Nicolas Olenev <cardinal2113@gmail.com>
2021-12-02 15:25:54 +0300

    add to_do_list UI

---
+++ marathon/to_do_list/js/main.js
@@ -0,0 +1,35 @@
+const list = [];
+const STATUSES = ['To Do', 'In Progress', 'Done'];
+let ID = 0;
+
+function addTask(task, priority = 'low') {
+  list.push({
+    id: ++ID,
+    name: task,
+    status: 'To Do',
+    priority: priority,
+  });
+}
+
+function changeStatus(ID, status) {
+  list.find(task => task.id === ID).status = status;
+}
+
+function deleteTask(ID) {
+  list.splice(list.findIndex(task => task.id === ID), 1);
+}
+
+function showList() {
+  const sortedList = {};
+  STATUSES.forEach(status => sortedList[status] = []);
+  for (let task of list) {
+    sortedList[task.status].push(`id:${task.id} "${task.name}"`);
+  }
+  for (let status in sortedList) {
+    const isEmptyTaskList = !sortedList[status].length;
+    if (isEmptyTaskList) sortedList[status].push('-');
+    console.log(`${status}:\n ${sortedList[status].join('\n ')}`);
+  }
+}
+
+export { list, STATUSES, ID, addTask, deleteTask };
