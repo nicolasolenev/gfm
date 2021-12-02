9899dda816ca7d0996cb6be3bdb4a434afe74d06
Nicolas Olenev <cardinal2113@gmail.com>
2021-12-02 16:53:19 +0300

    add enter function

--- marathon/to_do_list/js/view.js
+++ marathon/to_do_list/js/view.js
@@ -2,35 +2,33 @@ import { list, STATUSES, ID, addTask, deleteTask } from './main.js'
 
 const newTaskTemplate = document.createElement('div');
 newTaskTemplate.className = 'todo_list__task';
-newTaskTemplate.insertAdjacentHTML('afterbegin', '<input type="checkbox" class="todo_list__checkbox"><input type = "text" class= "todo_list__input"><button class="todo_list__delete_button"><img class="todo_list__delete_button_icon" src="img/add-icon.svg"></button>');
+newTaskTemplate.insertAdjacentHTML('afterbegin', `
+<input type="checkbox" class="todo_list__checkbox">
+<span class="todo_list__input"></span>
+<button class="todo_list__delete_button">
+  <img class="todo_list__delete_button_icon" src="img/add-icon.svg">
+</button>`);
 
 const addTaskButtons = document.querySelectorAll('.todo_list__add_button');
-console.log(addTaskButtons);
 for (let button of addTaskButtons) {
-  button.addEventListener('click', addTaskButtonHendler);
+  button.addEventListener('click', addTaskHendler);
 }
 
-function addTaskButtonHendler() {
-  console.log(this);
+function addTaskHendler() {
   const thisList = this.parentElement.parentElement;
-  console.log(thisList);
   const task = this.parentElement.firstElementChild.value;
   const priority = this.parentElement.parentElement.firstElementChild.textContent;
   addTask(task, priority);
   this.parentElement.firstElementChild.value = null;
-  console.log(list);
   const newTask = newTaskTemplate.cloneNode('deep');
   newTask.setAttribute('id', ID);
-  newTask.querySelector('.todo_list__input').value = task;
-  newTask.querySelector('.todo_list__delete_button').addEventListener('click', deleteTaskButtonHendler);
+  newTask.querySelector('.todo_list__input').textContent = task;
+  newTask.querySelector('.todo_list__delete_button').addEventListener('click', deleteTaskHendler);
   newTask.querySelector('.todo_list__checkbox').addEventListener('click', checkboxTaskHendler);
-
-
-  console.log(newTask.querySelector('.todo_list__input'));
   thisList.append(newTask);
 }
 
-function deleteTaskButtonHendler() {
+function deleteTaskHendler() {
   this.parentElement.remove();
 }
 
@@ -38,7 +36,21 @@ function checkboxTaskHendler() {
   this.parentElement.classList.toggle('done');
 }
 
-// const inputs = document.querySelectorAll('.todo_list__input');
-// for (let input of inputs) {
-//   input.addEventListener('keydown', e => e.keyCode === 13 ? addTaskButtonHendler() : 0);
-// }
+const inputs = document.querySelectorAll('.todo_list__input');
+for (let input of inputs) {
+  input.addEventListener('keydown', function (e) {
+    if (e.keyCode === 13) {
+      const thisList = this.parentElement.parentElement;
+      const task = this.parentElement.firstElementChild.value;
+      const priority = this.parentElement.parentElement.firstElementChild.textContent;
+      addTask(task, priority);
+      this.parentElement.firstElementChild.value = null;
+      const newTask = newTaskTemplate.cloneNode('deep');
+      newTask.setAttribute('id', ID);
+      newTask.querySelector('.todo_list__input').textContent = task;
+      newTask.querySelector('.todo_list__delete_button').addEventListener('click', deleteTaskHendler);
+      newTask.querySelector('.todo_list__checkbox').addEventListener('click', checkboxTaskHendler);
+      thisList.append(newTask);
+    }
+  });
+}
