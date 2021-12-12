6887d09071f367047886f66cd112b7125eb0baf3
Nicolas Olenev <cardinal2113@gmail.com>
2021-12-12 03:38:28 +0300

    to review

--- marathon/genderize/js/country.js
+++ marathon/genderize/js/country.js
@@ -1,4 +1,4 @@
-export const countries = {
+export default {
   'AF': 'Afghanistan',
   'AX': 'Aland Islands',
   'AL': 'Albania',
--- marathon/genderize/js/main.js
+++ marathon/genderize/js/main.js
@@ -1,47 +1,39 @@
-import { countries } from './country.js';
-
 function getElem(selector) {
   return document.querySelector(selector);
 }
 
 const UI = {
-  'form': getElem('#genderize_form'),
-  'input': getElem('#firstName'),
-  'gender': getElem('#gender'),
-  'country': getElem('#country'),
+  FORM: getElem('#genderize_form'),
+  INPUT: getElem('#firstName'),
+  GENDER: getElem('#gender'),
 }
 
-UI.form.addEventListener('submit', function () {
-  const firstName = UI.input.value.trim();
+const serverUrl = 'https://api.genderize.io';
+const errorText = 'No information or wrong name';
+
+UI.FORM.addEventListener('submit', function () {
+  const firstName = UI.INPUT.value.trim();
   const isNotEmptyName = firstName !== '';
 
   if (isNotEmptyName) {
-    UI.form.reset();
-    UI.gender.textContent = '';
-    UI.country.textContent = '';
-
-    const serverUrl_1 = 'https://api.genderize.io';
-    const url_1 = `${serverUrl_1}?name=${firstName}`;
-
-    const serverUrl_2 = 'https://api.nationalize.io';
-    const url_2 = `${serverUrl_2}?name=${firstName}`;
-
-    fetch(url_1).then(response => response.json()).then(response => {
-      const isGenderDefined = response.gender;
-
-      if (isGenderDefined) {
-        UI.gender.textContent = `${response.name[0].toUpperCase() + response.name.slice(1)} is ${response.gender}`;
-        fetch(url_2).then(response => response.json()).then(response => {
-          const isNotEmptyArrayOfCountries = response.country.length !== 0;
-          if (isNotEmptyArrayOfCountries)
-            return UI.country.textContent = `from ${countries[response.country[0].country_id]}`
-        });
-      } else {
-        UI.gender.textContent = 'No information or wrong name';
-      }
-
-    });
-
+    UI.FORM.reset();
+    UI.GENDER.textContent = '';
+    const url = `${serverUrl}?name=${firstName}`;
+    fetch(url)
+      .then(response => response.json())
+      .then(function (responseJson) {
+        const isGenderUndetermined = !responseJson.gender;
+        if (isGenderUndetermined) {
+          throw new Error(errorText);
+        }
+        UI.GENDER.textContent = `${responseJson.name[0].toUpperCase() + responseJson.name.slice(1)} is ${responseJson.gender}`;
+      })
+      .catch(err => {
+        if (err.message === errorText) {
+          UI.GENDER.textContent = err.message;
+        } else {
+          throw err;
+        }
+      });
   }
-
 });
